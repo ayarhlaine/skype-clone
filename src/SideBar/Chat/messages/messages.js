3f4facab7d2bbcb520d1a92654d21e64526fcc87
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2020-10-11 01:57:01 +0630

    ✨ Update User Messages

--- src/SideBar/Chat/messages/messages.js
+++ src/SideBar/Chat/messages/messages.js
@@ -9,13 +9,13 @@ export const chatMessages = [
     {
         imageUrl: 'https://scontent.frgn7-2.fna.fbcdn.net/v/t1.0-9/118952599_1359086711149542_8168842750560222428_n.jpg?_nc_cat=106&_nc_sid=09cbfe&_nc_eui2=AeEV3WuJXTuniZwW9FnWGwUOOHKPPTDuSgA4co89MO5KADHBkvjF8UZPUHXBx00FOurc7LI8X5w8781Gq4m0T6FU&_nc_ohc=20wjJm3lVFAAX9VspFm&_nc_ht=scontent.frgn7-2.fna&oh=1c49ff4947199345d68343b0f80a47af&oe=5FA975C9',
         name: 'Âunĝ Kÿaw Ĥtwē',
-        lastMessage: "Nice to meet you!",
+        lastMessage: "Hey bro, what are you doing?",
         lastActiveTime: '10/2/2020'
     },
     {
         imageUrl: 'https://scontent.frgn7-2.fna.fbcdn.net/v/t1.0-9/120654480_2661940347468958_3852826309694575273_o.jpg?_nc_cat=104&_nc_sid=09cbfe&_nc_eui2=AeEqYYIn7e-eTTwlcHCNBftGTKlHJUILzhpMqUclQgvOGjQUqjB1HwvPD1L7MDeRfOhE9vFbeeH8M0rCjOYwdj8F&_nc_ohc=JGXGC9B4nTwAX95BbrR&_nc_ht=scontent.frgn7-2.fna&oh=da181f91f1e9e950969b3c06d3c8e11d&oe=5FA7253B',
         name: 'Soe Arkar Htet',
-        lastMessage: "Nice to meet you!",
+        lastMessage: "Hey, let's go outside!",
         lastActiveTime: '10/2/2020',
     },
     {
