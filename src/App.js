b44791b1b6a3f6e28b31610858e86ea68fc3c459
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2020-10-10 19:53:49 +0630

    ✨ Format app Sidebar & Message

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import './App.css';
+import SideBar from './SideBar/SideBar';
+import Message from './Message/Message';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      <h1>Skype Clone App</h1>
+    <div className="app">
+      <div className="app__body">
+        <SideBar />
+        <Message />
+      </div>
     </div>
   );
 }
---
+++ src/SideBar/SideBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './SideBar.scss';
+const SideBar = () => {
+    return (
+        <div className='sidebar'>
+            Sidebar
+        </div>
+    )
+}
+
+export default SideBar
