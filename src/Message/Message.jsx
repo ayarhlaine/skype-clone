941f44c1e85b1bb765250ba66749eb4d657e063c
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2020-10-11 15:20:16 +0630

    ✨ Added Message Header

--- src/Message/Message.jsx
+++ src/Message/Message.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React from 'react';
+import MessageHeader from './MessageHeader/MessageHeader';
 import './Message.scss';
 const Message = () => {
     return (
         <div className='message'>
-            
+            <MessageHeader />
+            <hr className='message__divider'/>
         </div>
     )
 }
---
+++ src/Message/MessageHeader/MessageHeader.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { IconButton } from '@material-ui/core';
+import VideocamOutlinedIcon from '@material-ui/icons/VideocamOutlined';
+import CallOutlinedIcon from '@material-ui/icons/CallOutlined';
+import PersonAddOutlinedIcon from '@material-ui/icons/PersonAddOutlined';
+import PhotoLibraryOutlinedIcon from '@material-ui/icons/PhotoLibraryOutlined';
+import SearchOutlinedIcon from '@material-ui/icons/SearchOutlined';
+import FiberManualRecordIcon from '@material-ui/icons/FiberManualRecord';
+import './MessageHeader.scss';
+const MessageHeader = () => {
+    return (
+        <div className="messageHeader">
+            <div className="messageHeader__left">
+                <h3 className="messageHeader__left__title">
+                    Soe Arkar Htet
+                </h3>
+                <div className="messageHeader__left__content">
+                    <div className="messageHeader__left__content__container">
+                        <FiberManualRecordIcon className="messageHeader__left__content__active__status"/>
+                        <span>Web Developer</span>
+                    </div>
+                    <div class="vl"/>
+                    <div className="messageHeader__left__content__container">
+                        <PhotoLibraryOutlinedIcon />
+                        <span>Gallery</span>
+                    </div>
+                    <div class="vl"/>
+                    <div className="messageHeader__left__content__container">
+                        <SearchOutlinedIcon />
+                        <span>Find</span>
+                    </div>
+                        
+                </div>
+            </div>
+            <div className="messageHeader__right">
+                <IconButton className='messageHeader__right__button'>
+                    <VideocamOutlinedIcon />
+                </IconButton>
+                <IconButton className='messageHeader__right__button'>
+                    <CallOutlinedIcon />
+                </IconButton>
+                <IconButton className='messageHeader__right__button'>
+                    <PersonAddOutlinedIcon />
+                </IconButton>
+            </div>
+        </div>
+    )
+}
+
+export default MessageHeader
