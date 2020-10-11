d7b333f961d564cf2e331e202777874ff68eae4e
Ayar Hlaine <ayarhlaine.dev@gmail.com>
2020-10-11 15:27:04 +0630

    ✨ Changed Icons to outline

--- src/SideBar/MeetingButton/MeetingButton.jsx
+++ src/SideBar/MeetingButton/MeetingButton.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
-import VideocamIcon from '@material-ui/icons/Videocam';
-import BorderColorIcon from '@material-ui/icons/BorderColor';
+import VideocamOutlinedIcon from '@material-ui/icons/VideocamOutlined';
+import BorderColorOutlinedIcon from '@material-ui/icons/BorderColorOutlined';
 import KeyboardArrowDownIcon from '@material-ui/icons/KeyboardArrowDown';
 import './MeetingButton.scss';
 const MeetingButton = () => {
     return (
         <div className="meetingButton">
             <div className="meetingButton__button">
-                <VideocamIcon />
+                <VideocamOutlinedIcon />
                 <span>Meet Now</span>
                 <KeyboardArrowDownIcon />
             </div>
             <div className="meetingButton__button">
-                <BorderColorIcon />
+                <BorderColorOutlinedIcon />
                 <span>New Chat</span>
                 <KeyboardArrowDownIcon />
             </div>
--- src/SideBar/Tabs/Tabs.jsx
+++ src/SideBar/Tabs/Tabs.jsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react';
-import ChatIcon from '@material-ui/icons/Chat';
-import CallIcon from '@material-ui/icons/Call';
-import ContactsIcon from '@material-ui/icons/Contacts';
-import NotificationsIcon from '@material-ui/icons/Notifications';
+import InsertCommentOutlinedIcon from '@material-ui/icons/InsertCommentOutlined';
+import PhoneOutlinedIcon from '@material-ui/icons/PhoneOutlined';
+import ContactsOutlinedIcon from '@material-ui/icons/ContactsOutlined';
+import NotificationsOutlinedIcon from '@material-ui/icons/NotificationsOutlined';
 import './Tabs.scss';
 const tabs = {
     chat: 1,
@@ -19,25 +19,25 @@ const Tabs = () => {
                 <div 
                 className={`tabs__tab ${activeTab === chat ? 'tabs__tab--active' : ''}`}
                 onClick={() => setActiveTab(chat)}>
-                    <ChatIcon />
+                    <InsertCommentOutlinedIcon />
                     <span>Chats</span>
                 </div>
                 <div 
                 className={`tabs__tab ${activeTab === call ? 'tabs__tab--active' : ''}`}
                 onClick={() => setActiveTab(call)}>
-                    <CallIcon />
+                    <PhoneOutlinedIcon />
                     <span>Calls</span>
                 </div>
                 <div 
                 className={`tabs__tab ${activeTab === contact ? 'tabs__tab--active' : ''}`}
                 onClick={() => setActiveTab(contact)}>
-                    <ContactsIcon />
+                    <ContactsOutlinedIcon />
                     <span>Contacts</span>
                 </div>
                 <div 
                 className={`tabs__tab ${activeTab === notification ? 'tabs__tab--active' : ''}`}
                 onClick={() => setActiveTab(notification)}>
-                    <NotificationsIcon />
+                    <NotificationsOutlinedIcon />
                     <span>Notifications</span>
                 </div>
             </div>
