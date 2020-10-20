4bc64780b24bd7bf520d6d4575a7fe5657eedcf6
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-10-20 14:08:07 +0000

    Deploy ayarhlaine/skype-clone to github.com/ayarhlaine/skype-clone.git:gh-pages

---
+++ precache-manifest.8674ad258cb47bda48fb3f687e6b9334.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1df728a9ea34ed47b3977b676c5f686e",
+    "url": "/skype-clone/index.html"
+  },
+  {
+    "revision": "418f97010abc13792f1e",
+    "url": "/skype-clone/static/css/main.452e59e3.chunk.css"
+  },
+  {
+    "revision": "3b77064baaf873ce8993",
+    "url": "/skype-clone/static/js/2.9332b9fa.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/skype-clone/static/js/2.9332b9fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "418f97010abc13792f1e",
+    "url": "/skype-clone/static/js/main.c7f161c9.chunk.js"
+  },
+  {
+    "revision": "1882a7f8391866659ec8",
+    "url": "/skype-clone/static/js/runtime-main.8ba2b40b.js"
+  },
+  {
+    "revision": "ef272a582d97cd5b59e00526f0694351",
+    "url": "/skype-clone/static/media/Tahoma-Regular.ef272a58.ttf"
+  }
+]);
